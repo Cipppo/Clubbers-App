1687b136743ab4db1ab12a9483af2d0217548459
Michele Montesi <michelemontesi36@gmail.com>
2023-04-27 15:26:46 +0200

    Added adminsDAO to DAOs

--- app/src/main/java/com/example/clubbers/data/ClubbersDatabase.kt
+++ app/src/main/java/com/example/clubbers/data/ClubbersDatabase.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import com.example.clubbers.data.dao.AdminDAO
 import com.example.clubbers.data.dao.UsersDAO
 import com.example.clubbers.data.entities.Admin
 import com.example.clubbers.data.entities.Event
@@ -18,7 +19,10 @@ import com.example.clubbers.data.entities.User
 @Database(entities = [User::class, Admin::class, Post::class, Event::class], version = 1, exportSchema = true)
 abstract class ClubbersDatabase : RoomDatabase() {
 
+    // DAOs
     abstract fun usersDAO(): UsersDAO
+    abstract fun adminsDAO(): AdminDAO
+    // TODO: insert other DAOs here
 
     companion object {
         @Volatile
