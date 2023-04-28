25f4df1de553c4a46d116340dd180ed34ea6dd4b
Michele Montesi <michele.montesi3@studio.unibo.it>
2023-04-28 10:42:48 +0200

    Created DataModule file

---
+++ app/src/main/java/com/example/clubbers/di/DataModule.kt
@@ -0,0 +1,11 @@
+package com.example.clubbers.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DataModule {
+    // TODO: Create repositories and then provide them here
+}
