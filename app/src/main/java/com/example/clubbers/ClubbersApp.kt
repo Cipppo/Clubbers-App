f12515fa06116f66c22ad41da4dc3da1f2cdc7b4
Michele Montesi <michele.montesi3@studio.unibo.it>
2023-04-28 12:26:29 +0200

    Created Hilt android app

--- app/src/main/java/com/example/clubbers/ClubbersApp.kt
+++ app/src/main/java/com/example/clubbers/ClubbersApp.kt
@@ -1,18 +1,29 @@
 package com.example.clubbers
 
-sealed class ClubbersApp(val name: String) {
+import android.app.Application
+import com.example.clubbers.data.ClubbersDatabase
+import dagger.hilt.android.HiltAndroidApp
+
+sealed class AppScreen(val name: String) {
     // Bottom Bar
-    object Home : ClubbersApp("Home")
-    object NewPost : ClubbersApp("Create Post")
-    object Discover : ClubbersApp("Discover")
-    object Profile : ClubbersApp("Personal Profile")
-    object Today : ClubbersApp("Today's Events")
+    object Home : AppScreen("Home")
+    object NewPost : AppScreen("Create Post")
+    object Discover : AppScreen("Discover")
+    object Profile : AppScreen("Personal Profile")
+    object Today : AppScreen("Today's Events")
 
     // Other Screens
-    object Event : ClubbersApp("Event Details")
-    object Settings : ClubbersApp("Settings Screen")
-    object User : ClubbersApp("User Profile")
+    object Event : AppScreen("Event Details")
+    object Settings : AppScreen("Settings Screen")
+    object User : AppScreen("User Profile")
 
     // TODO: If there will be more screens, add them here
 }
+
+@HiltAndroidApp
+class ClubbersApp : Application() {
+    // lazy --> the database and the repository are only created when they're needed
+    val database by lazy { ClubbersDatabase.getDatabase(this) }
+}
+
 
