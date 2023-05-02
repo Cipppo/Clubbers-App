4f9df83667ff935740cb45bc93123df8dc608057
Michele Montesi <michelemontesi36@gmail.com>
2023-05-02 15:57:54 +0200

    Created NavigationGraph and BottomAppBar
    Created HomeScreen white page

--- app/src/main/java/com/example/clubbers/ClubbersApp.kt
+++ app/src/main/java/com/example/clubbers/ClubbersApp.kt
@@ -1,7 +1,29 @@
 package com.example.clubbers
 
 import android.app.Application
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.BottomAppBarDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.FloatingActionButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.clubbers.data.ClubbersDatabase
+import com.example.clubbers.ui.HomeScreen
 import dagger.hilt.android.HiltAndroidApp
 
 sealed class AppScreen(val name: String) {
@@ -26,4 +48,61 @@ class ClubbersApp : Application() {
     val database by lazy { ClubbersDatabase.getDatabase(this) }
 }
 
+@Composable
+fun BottomAppBarFunction (
+    currentScreen: String,
+    modifier: Modifier = Modifier
+) {
+    BottomAppBar (
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(Icons.Filled.Home, contentDescription = "Add Post")
+            }
+        },
+        modifier = modifier,
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = { /*TODO*/ },
+                containerColor = BottomAppBarDefaults.bottomAppBarFabColor,
+                elevation = FloatingActionButtonDefaults.bottomAppBarFabElevation()
+            ) {
+                Icon(Icons.Filled.Add, contentDescription = "Add Post")
+            }
+        }
+    )
+}
 
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NavigationApp (
+    navController: NavHostController = rememberNavController()
+) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = backStackEntry?.destination?.route ?: AppScreen.Home.name
+
+    Scaffold(
+        bottomBar = {
+            BottomAppBarFunction(currentScreen = currentScreen)
+        }
+    ) { innerPadding ->
+        NavigationGraph(navController, innerPadding)
+    }
+}
+
+@Composable
+private fun NavigationGraph(
+    navController: NavHostController,
+    innerPadding: PaddingValues,
+    modifier: Modifier = Modifier
+) {
+    NavHost(
+        navController = navController,
+        startDestination = AppScreen.Home.name,
+        modifier = modifier.padding(innerPadding)
+    ) {
+        // Home Screen
+        composable(route = AppScreen.Home.name) {
+            HomeScreen()
+        }
+    }
+}
--- app/src/main/java/com/example/clubbers/ui/HomeScreen.kt
+++ app/src/main/java/com/example/clubbers/ui/HomeScreen.kt
@@ -1,2 +1,20 @@
 package com.example.clubbers.ui
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun HomeScreen(
+    modifier: Modifier = Modifier
+) {
+    Scaffold { innerPadding ->
+        Column (modifier.padding(innerPadding)) {
+
+        }
+    }
+}
