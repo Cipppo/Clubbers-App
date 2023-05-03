f36205985164d50abba77f44c1f34053db529e6f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-05-03 16:18:11 +0200

    Implementing RegistrationPage

--- app/src/main/java/com/example/clubbers/ui/RegistrationScreen.kt
+++ app/src/main/java/com/example/clubbers/ui/RegistrationScreen.kt
@@ -1,5 +1,67 @@
 package com.example.clubbers.ui
 
-fun RegistrationScreen(){
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+@Preview
+fun RegistrationScreen(){
+    Column(
+        modifier = Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+            .background(Color.White),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ){
+        val firstName = remember { mutableStateOf(TextFieldValue()) }
+        val secondName = remember { mutableStateOf(TextFieldValue()) }
+        val email = remember { mutableStateOf(TextFieldValue()) }
+        val password = remember { mutableStateOf(TextFieldValue()) }
+
+
+        Text(
+            text = "Are you ready ?",
+            style = TextStyle(
+                fontSize = 30.sp
+            )
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "First Name") },
+            value = firstName.value,
+            onValueChange = {firstName.value = it}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Second Name") },
+            value = secondName.value,
+            onValueChange = {secondName.value = it}
+        )
+    }
 }
