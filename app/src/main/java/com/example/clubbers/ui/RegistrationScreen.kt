47a6aa359863b987f4b122717edaaad9e0f873b1
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-05-04 14:55:57 +0200

    Implemented registration page and form controls

--- app/src/main/java/com/example/clubbers/ui/RegistrationScreen.kt
+++ app/src/main/java/com/example/clubbers/ui/RegistrationScreen.kt
@@ -1,5 +1,6 @@
 package com.example.clubbers.ui
 
+import android.util.Log
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
@@ -9,16 +10,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.input.TextFieldValue
@@ -43,7 +47,24 @@ fun RegistrationScreen(){
         val secondName = remember { mutableStateOf(TextFieldValue()) }
         val email = remember { mutableStateOf(TextFieldValue()) }
         val password = remember { mutableStateOf(TextFieldValue()) }
+        val passwordConf = remember { mutableStateOf(TextFieldValue()) }
+        var isButtonEnabled = remember { mutableStateOf(true) }
 
+        LaunchedEffect(
+            firstName,
+            secondName,
+            email,
+            password,
+            passwordConf,
+        ) {
+            isButtonEnabled = mutableStateOf(
+                    firstName.value.text.isNotEmpty() &&
+                            secondName.value.text.isNotEmpty() &&
+                            email.value.text.isNotEmpty() &&
+                            password.value.text.isNotEmpty() &&
+                            passwordConf.value.text.isNotEmpty()
+                    )
+        }
 
         Text(
             text = "Are you ready ?",
@@ -55,13 +76,52 @@ fun RegistrationScreen(){
         TextField(
             label = { Text(text = "First Name") },
             value = firstName.value,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
             onValueChange = {firstName.value = it}
         )
         Spacer(modifier = Modifier.height(20.dp))
         TextField(
             label = { Text(text = "Second Name") },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
             value = secondName.value,
             onValueChange = {secondName.value = it}
         )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Email")},
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Send),
+            value = email.value,
+            onValueChange = {email.value = it}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = { Text(text = "Password")},
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Send),
+            value = password.value,
+            onValueChange = {password.value = it},
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        TextField(
+            label = {Text(text = "Confirm Password")},
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password, imeAction = ImeAction.Send),
+            value = passwordConf.value,
+            onValueChange = {passwordConf.value = it}
+        )
+        Spacer(modifier = Modifier.height(20.dp))
+        Button(
+            onClick = { Log.d("SENDBUTTON", firstName.value.text.toString()) },
+            enabled = EntriesCheck(firstName.value.text, secondName.value.text, email.value.text, password.value.text, passwordConf.value.text)
+        ) {
+           Text("Register")
+        }
     }
+}
+
+fun EntriesCheck(firstname: String, secondName: String, email: String, password: String, confirmPassword: String): Boolean{
+    if(firstname.isNotBlank() && secondName.isNotBlank() && email.isNotBlank() && password.isNotBlank() && confirmPassword.isNotBlank()){
+        return true
+    }
+    return false
 }
