c30c10e152ffb2498240221a0b85712b743a837f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-05-08 16:30:44 +0200

    Change sharedPreferences call method

--- app/src/main/java/com/example/clubbers/ui/LoginScreen.kt
+++ app/src/main/java/com/example/clubbers/ui/LoginScreen.kt
@@ -32,6 +32,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.textInputServiceFactory
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.AnnotatedString
@@ -61,7 +62,6 @@ fun LoginScreen(
     switchToRegister: () -> Unit,
     onLogin: () -> Unit,
     usersViewModel: UsersViewModel,
-    sharedPreferences: SharedPreferences
 ){
     Box(
         modifier = Modifier.fillMaxSize()
@@ -84,6 +84,8 @@ fun LoginScreen(
         val username = remember { mutableStateOf(TextFieldValue()) }
         val password = remember { mutableStateOf(TextFieldValue()) }
         val usersList = usersViewModel.getAllUsers().collectAsState(initial = listOf()).value
+        val sharedPreferences = LocalContext.current.getSharedPreferences("USER_LOGGED", Context.MODE_PRIVATE)
+
 
 
         Text(
