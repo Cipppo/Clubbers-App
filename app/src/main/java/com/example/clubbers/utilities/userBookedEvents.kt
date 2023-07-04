33534379e1f5ce06ca888c94edc4140fb6e0e693
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-07-04 21:24:34 +0200

    Making userBooked feed non static

--- app/src/main/java/com/example/clubbers/utilities/userBookedEvents.kt
+++ app/src/main/java/com/example/clubbers/utilities/userBookedEvents.kt
@@ -10,23 +10,38 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.compose.rememberImagePainter
+import coil.request.ImageRequest
 import com.example.clubbers.R
+import com.example.clubbers.viewModel.EventsViewModel
+import com.example.clubbers.viewModel.ParticipatesViewModel
 
 
 @Composable
-fun userBookedEvents(){
-    
-    val events = listOf<Int>(1, 2, 3, 4, 5)
+fun userBookedEvents(
+    userId: Int,
+    participatesViewModel: ParticipatesViewModel,
+    eventsViewModel: EventsViewModel,
+){
+
+    participatesViewModel.getEvents(userId = userId)
+    val events = participatesViewModel.events.collectAsState().value
     
     
     LazyColumn(
@@ -35,6 +50,8 @@ fun userBookedEvents(){
             .fillMaxHeight()
     ){
         items(events){event ->
+            eventsViewModel.selectEvent(event)
+            val selectedEvent = eventsViewModel.eventSelected
             Card(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -46,18 +63,28 @@ fun userBookedEvents(){
                         modifier = Modifier.padding(bottom = 8.dp)
                     ) {
                         Spacer(modifier = Modifier.height(2.dp))
-                        Image(
-                            painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                            contentDescription = "Evento",
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .border(
-                                    1.dp,
-                                    MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
-                                    shape = MaterialTheme.shapes.small
-                                )
-                                .heightIn(min = 100.dp)
-                        )
+                        if (selectedEvent != null) {
+                            Image(
+                                painter = rememberAsyncImagePainter(
+                                    ImageRequest.Builder(
+                                        LocalContext.current
+                                    ).data(data = selectedEvent.eventImage).apply(block = fun ImageRequest.Builder.() {
+                                        crossfade(true)
+                                        placeholder(R.drawable.ic_launcher_foreground)
+                                        error(R.drawable.ic_launcher_foreground)
+                                    }).build()
+                                ),
+                                contentDescription = "Event Banner",
+                                modifier =  Modifier
+                                    .fillMaxWidth()
+                                    .border(
+                                        1.dp,
+                                        MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+                                        shape = MaterialTheme.shapes.small
+                                    )
+                                    .heightIn(min = 100.dp)
+                            )
+                        }
                     }
                 }
             }
