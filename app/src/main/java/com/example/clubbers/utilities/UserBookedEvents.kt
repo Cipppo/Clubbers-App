93f4d51c59ace92790c979e3d4cdc9535a7c0411
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-07-11 06:01:05 +0200

    Solved bug

---
+++ app/src/main/java/com/example/clubbers/utilities/UserBookedEvents.kt
@@ -0,0 +1,88 @@
+package com.example.clubbers.utilities
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
+import coil.request.ImageRequest
+import com.example.clubbers.R
+import com.example.clubbers.viewModel.EventsViewModel
+import com.example.clubbers.viewModel.ParticipatesViewModel
+
+@Composable
+fun UserBookedEvents(
+    userId: Int,
+    participatesViewModel: ParticipatesViewModel,
+    eventsViewModel: EventsViewModel,
+){
+
+    participatesViewModel.getEvents(userId = userId)
+    val events = participatesViewModel.events.collectAsState().value
+
+
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ){
+        items(events){event ->
+            eventsViewModel.selectEvent(event)
+            val selectedEvent = eventsViewModel.eventSelected
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp, vertical = 8.dp),
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    ) {
+                        Spacer(modifier = Modifier.height(2.dp))
+                        if (selectedEvent != null) {
+                            Image(
+                                painter = rememberAsyncImagePainter(
+                                    ImageRequest.Builder(
+                                        LocalContext.current
+                                    ).data(data = selectedEvent.eventImage).apply(block = fun ImageRequest.Builder.() {
+                                        crossfade(true)
+                                        placeholder(R.drawable.ic_launcher_foreground)
+                                        error(R.drawable.ic_launcher_foreground)
+                                    }).build()
+                                ),
+                                contentDescription = "Event Banner",
+                                modifier =  Modifier
+                                    .fillMaxWidth()
+                                    .border(
+                                        1.dp,
+                                        MaterialTheme.colorScheme.onSurface.copy(alpha = 0.12f),
+                                        shape = MaterialTheme.shapes.small
+                                    )
+                                    .heightIn(min = 100.dp)
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+
+}
