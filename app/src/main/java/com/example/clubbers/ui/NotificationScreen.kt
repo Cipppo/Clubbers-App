c0da7256db079e83eabde7c07b34cd5b0243bd4f
Luca Rapolla <luca.rapolla@studio.unibo.it>
2023-08-18 21:27:49 +0200

    Handling notifications Screen

--- app/src/main/java/com/example/clubbers/ui/NotificationScreen.kt
+++ app/src/main/java/com/example/clubbers/ui/NotificationScreen.kt
@@ -1,9 +1,11 @@
 package com.example.clubbers.ui
 
 import android.content.Context
+import android.util.Log
 import android.widget.Space
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -12,6 +14,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -28,17 +31,62 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import com.example.clubbers.R
+import com.example.clubbers.data.entities.Admin
 import com.example.clubbers.data.entities.Notification
+import com.example.clubbers.data.entities.User
+import com.example.clubbers.viewModel.AdminsViewModel
 import com.example.clubbers.viewModel.NotificationsViewModel
+import com.example.clubbers.viewModel.UserFollowsAdminsViewModel
 import com.example.clubbers.viewModel.UserFollowsUsersViewModel
 import com.example.clubbers.viewModel.UsersViewModel
+
+
+
+
+@Composable
+fun NotificationScreenRouter(
+    modifier: Modifier,
+    usersViewModel: UsersViewModel,
+    notificationsViewModel: NotificationsViewModel,
+    adminsViewModel: AdminsViewModel
+){
+
+    val userEmail = LocalContext.current.getSharedPreferences("USER_LOGGED", Context.MODE_PRIVATE).getString("USER_LOGGED", "None")
+    val userType = LocalContext.current.getSharedPreferences("USER_LOGGED", Context.MODE_PRIVATE).getString("USER_TYPE", "NONE").orEmpty()
+    if(userType == "USER"){
+        usersViewModel.getUserByEmail(userEmail.orEmpty())
+        val user by usersViewModel.userByMail.collectAsState()
+        NotificationScreen(
+            modifier = modifier,
+            usersViewModel = usersViewModel,
+            notificationsViewModel = notificationsViewModel,
+            adminsViewModel = adminsViewModel,
+            user = user
+        )
+    }else if(userType == "ADMIN"){
+        adminsViewModel.getAdminByMail(userEmail.orEmpty())
+        val user by adminsViewModel.adminByMail.collectAsState()
+        NotificationScreen(
+            modifier = modifier,
+            usersViewModel = usersViewModel,
+            notificationsViewModel = notificationsViewModel,
+            adminsViewModel = adminsViewModel,
+        )
+    }
+
+
+}
+
+// DIVIDERE LA NOTIFICATIONSCREEN PER ADMIN E PER USER 
 
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
@@ -46,14 +94,23 @@ import com.example.clubbers.viewModel.UsersViewModel
 fun NotificationScreen(
     modifier: Modifier,
     usersViewModel: UsersViewModel,
-    notificationsViewModel: NotificationsViewModel){
+    notificationsViewModel: NotificationsViewModel,
+    adminsViewModel: AdminsViewModel,
+    user: User?){
 
-    val userEmail = LocalContext.current.getSharedPreferences("USER_LOGGED", Context.MODE_PRIVATE).getString("USER_LOGGED", "None")
-    usersViewModel.getUserByEmail(userEmail.orEmpty())
-    val user by usersViewModel.userByMail.collectAsState()
+
+
+
+
     val userId = user?.userId
+
+
+
     notificationsViewModel.getAllUserNotifications(user?.userId.toString().toInt())
     var notifications = notificationsViewModel.allUserNotifications.collectAsState().value.reversed()
+
+
+
 
     Scaffold(modifier = modifier) { innerPadding ->
         Spacer(modifier = Modifier.height(10.dp))
@@ -65,36 +122,50 @@ fun NotificationScreen(
                 stickyHeader {
                 }
                 items(notifications.size) { index ->
-                    notificationItem(modifier = modifier, notification = notifications[index], usersViewModel)
+                    notificationItem(modifier = modifier, notification = notifications[index], usersViewModel, user)
                 }
             }
         )
     }
 }
 
+
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun notificationItem(
     modifier: Modifier,
     notification: Notification,
-    usersViewModel: UsersViewModel
+    usersViewModel: UsersViewModel,
+    user: User?
 ){
+
+
     Row(
         modifier = Modifier
             .fillMaxWidth()
             .padding(2.dp),
         ) {
-            if(notification.notification_type == "SUBSCRIPTION"){
-                Row(Modifier.fillMaxWidth()){
-                    Text(text = "NUOVA ISCRIZIONE\n${notification.message}", fontWeight = FontWeight.Bold)
+            if(user?.isAdmin == false){
+                if(notification.notification_type == "FOLLOW"){
+                    usersViewModel.getUserById(notification.senderId)
+                    var from = usersViewModel.userById.collectAsState().value?.userUsername
+                    Row(Modifier.fillMaxWidth()){
+                        Text(text = "NUOVO FOLLOWER!\n${from} ${notification.message}", fontWeight = FontWeight.Bold)
+                    }
                 }
-            }else if(notification.notification_type == "FOLLOW"){
-                usersViewModel.getUserById(notification.senderId)
-                var from = usersViewModel.userById.collectAsState().value?.userUsername
-                Row(Modifier.fillMaxWidth()){
-                    Text(text = "NUOVO FOLLOWER!\n${from} ${notification.message}", fontWeight = FontWeight.Bold)
+            }else{
+                if(notification.notification_type == "SUBSCRIPTION") {
+                    Row(Modifier.fillMaxWidth()) {
+                        Text(
+                            text = "NUOVA ISCRIZIONE\n${notification.message}",
+                            fontWeight = FontWeight.Bold
+                        )
+                    }
                 }
             }
+
 
     }
     Spacer(modifier = Modifier.height(10.dp))
